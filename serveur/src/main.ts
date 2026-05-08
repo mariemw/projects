1de50e43f711516c54b659804aad0cc6ac2dc0e2
mariem <mariem.wadhen@polymtl.ca>
2026-05-08 16:04:16 -0400

    fix deploy

--- serveur/src/main.ts
+++ serveur/src/main.ts
@@ -5,10 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:4200',
-      'https://projects-q7zw.vercel.app'
-    ],
+    origin:true,
     credentials: true,
   });
 
